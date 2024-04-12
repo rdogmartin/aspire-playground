500d720c5d6b99309d2dde092791dbc1da211665
Roger Martin <rmartin@youraspire.com>
2024-04-12 17:14:17 -0600

    Added ContactController

---
+++ Aspire.WebApi/Controllers/ContactController.cs
@@ -0,0 +1,35 @@
+﻿using System.Diagnostics;
+using System.Reflection;
+using Aspire.WebApi.Dtos;
+using Aspire.WebApi.ValueObjects;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Aspire.WebApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+[AllowAnonymous]
+public class ContactController : ControllerBase
+{
+    [HttpGet("{id}")]
+    [Produces("application/json")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public ActionResult<Contact> Get(int id)
+    {
+        return new Contact(id, "John Doe", "none@nospam.com");
+    }
+
+    [HttpPost]
+    [Produces("application/json")]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public ActionResult<Contact> Post(Contact contact)
+    {
+        // TODO: Persist to DB
+        contact = contact with { Id = 1 };
+
+        return CreatedAtAction(nameof(Get), new { id = contact.Id }, contact);
+    }
+}
---
+++ Aspire.WebApi/Dtos/Contact.cs
@@ -0,0 +1,5 @@
+﻿namespace Aspire.WebApi.Dtos;
+
+public record class Contact(int Id, string Name, string Email)
+{
+}
